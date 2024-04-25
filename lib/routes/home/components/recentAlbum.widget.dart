a014f196c2c82a013b24003aae3c20f2147abba6
소희 <109736890+SO-HUII@users.noreply.github.com>
2024-04-25 11:47:19 +0900

    Feat: recentAlbum widget 구현 및 homeview에 적용

---
+++ lib/routes/home/components/recentAlbum.widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:music_app_clone_coding/common/style/font.dart';
+
+class RecentAlbum extends StatelessWidget {
+  const RecentAlbum({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "최신 앨범",
+                style: MyFontFamily.subTitle,
+              ),
+              Text(
+                "더보기",
+                style: MyFontFamily.detailTitle,
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: 210,
+          child: Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 150,
+                  //height: 250,
+                  margin: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.lightGreen,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        "노래 제목",
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        "가수 이름",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
