6487ac8b4e5a77ef52e901bfbae43fa80dd5e3a5
소희 <109736890+SO-HUII@users.noreply.github.com>
2024-05-17 09:01:28 +0900

    Feat: player view 추가

---
+++ lib/routes/player/player.view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PlayView extends StatelessWidget {
+  const PlayView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
