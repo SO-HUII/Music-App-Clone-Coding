b3fbb3cdfe9ad9a1395b8b2b8d0350a9f9cf32c3
소희 <109736890+SO-HUII@users.noreply.github.com>
2024-05-17 09:02:33 +0900

    Feat: player widget 추가

---
+++ lib/routes/player/components/musicPlay.widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicPlay extends StatelessWidget {
+  const MusicPlay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
