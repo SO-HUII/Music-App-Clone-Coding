5b4e3ff9d35b09c35f34c52aff3e5915d77f176f
소희 <109736890+SO-HUII@users.noreply.github.com>
2024-05-31 11:30:36 +0900

    Feat: music detail view 구현 및 search view와 연결, album 기본 이미지 변경

---
+++ lib/routes/player/musicDetail.view.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:music_app_clone_coding/routes/player/music.controller.dart';
+
+class MusicDetailView extends StatelessWidget {
+  final String tag, title, singer, composer, lyricist, release;
+  const MusicDetailView({
+    super.key,
+    required this.tag,
+    required this.title,
+    required this.singer,
+    required this.composer,
+    required this.lyricist,
+    required this.release,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final MusicController controller = MusicController.to;
+    controller.getMusics();
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.chevron_left,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: const [
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          SizedBox(width: 10),
+          Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+          SizedBox(width: 10),
+        ],
+      ),
+      body: Hero(
+        tag: tag,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.7),
+                        blurRadius: 5.0,
+                        spreadRadius: 0.0,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                    image: const DecorationImage(
+                      image: AssetImage('assets/images/album.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              textAlign: TextAlign.center,
+              singer,
+              style: const TextStyle(fontSize: 15),
+            ),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                button("재생하기"),
+                const SizedBox(width: 15),
+                button("프로필뮤직"),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "상세정보",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Divider(
+              height: 1,
+              color: Colors.grey.withOpacity(0.2),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 20,
+              ),
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    singer,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Divider(
+                    height: 1,
+                    color: Colors.grey.withOpacity(0.2),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget button(String name) {
+    return SizedBox(
+      width: 120,
+      child: OutlinedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          padding: WidgetStateProperty.all<EdgeInsets>(
+            const EdgeInsets.symmetric(horizontal: 22, vertical: 3),
+          ),
+          side: WidgetStateProperty.all(
+            const BorderSide(
+              color: Colors.grey,
+              width: 1.0,
+              style: BorderStyle.solid,
+            ),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            name == "재생하기"
+                ? const Icon(
+                    Icons.play_arrow_rounded,
+                    color: Colors.green,
+                  )
+                : Container(),
+            Text(
+              name,
+              style: TextStyle(
+                color: Colors.grey[600],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
